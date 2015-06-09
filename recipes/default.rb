84a4729681c74d7e1137b7fe6d3050d27c59f9c6
gotyoooo <lss.ken8927@gmail.com>
2015-06-09 19:37:16 +0900

    icu source download and install

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,50 @@
 #
-# Cookbook Name:: icu
+# Cookbook Name:: livicu
 # Recipe:: default
+# Author:: ken gotoh (lss.ken8927@gmail.com)
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2015, Ken Gotoh
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include_recipe "build-essential"
+
+version = node['libicu']['version']
+prefix = node['libicu']['prefix']
+
+version_underscore = version.sub(/./,"_")
+
+remote_file "#{Chef::Config[:file_cache_path]}/icu4c-#{version_underscore}-src.tgz" do
+  source "http://download.icu-project.org/files/icu4c/#{version}/icu4c-#{version_underscore}-src.tgz"
+  not_if {::File.exists?("#{prefix}/lib/icu/#{version}")}
+  notifies :run, "script[install-libicu]", :immediately
+end
+
+script "install-libicu" do
+  interpreter "bash"
+  only_if {::File.exists?("#{Chef::Config[:file_cache_path]}/icu4c-#{version_underscore}-src.tgz")}
+  flags "-e -x"
+  code <<-EOH
+    cd /usr/local/src
+    tar xzf #{Chef::Config[:file_cache_path]}/icu4c-#{version_underscore}-src.tgz
+    cd icu/source
+    ./configure --prefix=#{prefix}
+    make
+    make install
+  EOH
+end
+
+file "libicu-tarball-cleanup" do
+  path "#{Chef::Config[:file_cache_path]}/icu4c-#{version_underscore}-src.tgz"
+  action :delete
+end
