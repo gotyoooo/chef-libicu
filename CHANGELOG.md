69266e6abed33671838085d98c5e36e882e67c31
gotyoooo <lss.ken8927@gmail.com>
2015-06-10 10:48:11 +0900

    fix bug

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ libicu CHANGELOG
 
 This file is used to list changes made in each version of the ilivcu cookbook.
 
+0.1.1
+-----
+- [Ken Gotoh] - fix bug
+
 0.1.0
 -----
 - [Ken Gotoh] - Initial release of icu
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'lss.ken8927@gmail.com'
 license          'All rights reserved'
 description      'Installs livicu from source'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w{ fedora redhat centos ubuntu debian amazon }.each do |os|
   supports os
